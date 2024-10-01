22bf333a8149c5ab7db1339e06685c74a48087fc
Rohit <rohithrajka524@gmail.com>
2024-10-01 10:43:35 +0530

    completed

--- app.js
+++ app.js
@@ -9,10 +9,23 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    return (typeof(parents) === 'string' && typeof(noOfSiblings) === 'number' && typeof(isNuclearFamily) === 'boolean');
+}
+
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber) {
+    if (isNaN(ageInText)) {
+        return ageInText;
+    } else if (isNaN(ageInNumber)) {
+        return ageInNumber;
+    }
+    return null;
+}
+
 
 //Trial 4:
 //Lily gave Karen x sweets
@@ -21,17 +34,31 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    return (totalNoOfSweets-sweetsConsumedByKaren- sweetsConsumedInNMeters*metersToTravel)/2
+}
+
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelcius(f) {
+    let celsius = (f-32)*5/9;
+    return celsius
+}
 
 //Trial 6:
 //Lily can now do multiple things to deal with this
-//1. Take her daughter to a doctor
-//2. Talk to her husband about it
-//3. Counsel her daughter herself
-//4. Lock her daughter in her room
-//Given a value, return which of these above actions Lily would take
+// 1. Take her daughter to a doctor
+// 2. Talk to her husband about it
+// 3. Counsel her daughter herself
+// 4. Lock her daughter in her room
+//Given a value, return which of these above actions Lily would 
+
+function aDifficultChoice(n) {
+    const list =["Take her daughter to a doctor", "Talk to her husband about it", "Counsel her daughter herself", "Lock her daughter in her room"]
+    return list[n-1]
+}
+
 
 //Challenge 1:
 //Lily realized that she'd hurt her daughter
@@ -41,3 +68,8 @@ function moreAboutHome(address,distanceFromTown,hasNeighbours){
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+
+function consoleKaren(strategies) {
+    let strategy = strategies.join(' ');
+    return strategy.length;
+}
